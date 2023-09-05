1bf315b996af7fbe9e5bdd6949176f3e8327f456
KurtisIvey <kurtiveycodes@gmail.com>
2023-09-05 09:06:58 -0500

    update

---
+++ src/September05/DoubleChar.java
@@ -0,0 +1,15 @@
+package September05;
+
+public class DoubleChar {
+    public static String doubleChar(String s){
+        //enter your code here
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i< s.length();i++) { // iterate through w/ for loop
+            output.append(doubleIt(s.charAt(i))); // add doubled char to output stringbuilder
+        }
+        return output.toString();
+    }
+    public static String doubleIt(char c) {
+        return String.valueOf(c) + c; // converts char to string and doubles it
+    }
+}
