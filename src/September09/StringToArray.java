4f68248563ce740fa9758b96ad92ecd499182ef5
KurtisIvey <kurtiveycodes@gmail.com>
2023-09-09 18:05:50 -0500

    update

---
+++ src/September09/StringToArray.java
@@ -0,0 +1,8 @@
+package September09;
+
+public class StringToArray {
+    //Write a function to split a string and convert it into an array of words.
+    public static String[] stringToArray(String s) {
+        return s.split(" ");
+    }
+}
