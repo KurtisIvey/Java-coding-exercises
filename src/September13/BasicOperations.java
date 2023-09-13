ca5629b05cca327f9336f7ad8e1f915610278d8f
KurtisIvey <kurtiveycodes@gmail.com>
2023-09-13 12:40:56 -0500

    update

--- src/September13/BasicOperations.java
+++ src/September13/BasicOperations.java
@@ -3,39 +3,20 @@ package September13;
 public class BasicOperations {
     public static Integer basicMath(String op, int v1, int v2) {
         switch (op) {
-            case "+": {
-                return addition(v1, v2);
-            }
-            case "-": {
-                return subtraction(v1, v2);
-            }
-            case "*": {
-                return multiplication(v1, v2);
-            }
+            case "-":
+                return v1 - v2;
+            case "+":
+                return v1 + v2;
+            case "*":
+                return v1 * v2;
             case "/": {
-                return division(v1, v2);
+                if (v2 == 0)
+                    throw new IllegalArgumentException("Division by zero");
+                return v1 / v2;
             }
             default:
                 throw new IllegalArgumentException("Invalid operation: " + op);
         }
     }
 
-    public static Integer addition(int v1, int v2) {
-        return v1 + v2;
-    }
-
-    public static Integer subtraction(int v1, int v2) {
-        return v1 - v2;
-    }
-
-    public static Integer multiplication(int v1, int v2) {
-        return v1 * v2;
-    }
-
-    public static Integer division(int v1, int v2) {
-        if (v2 == 0) {
-            throw new ArithmeticException("Division by zero is not allowed.");
-        }
-        return v1 / v2;
-    }
 }
