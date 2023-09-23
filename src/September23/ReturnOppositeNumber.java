f783442276187abe8f3588a7fee998f365d37861
KurtisIvey <kurtiveycodes@gmail.com>
2023-09-23 16:55:04 -0500

    update

---
+++ src/September23/ReturnOppositeNumber.java
@@ -0,0 +1,9 @@
+package September23;
+
+public class ReturnOppositeNumber {
+    public static int opposite(int number)
+    {
+        // if pos, return negative of it. conversely, if num is negative mult by -1 will turn it positive
+        return number * -1;
+    }
+}
