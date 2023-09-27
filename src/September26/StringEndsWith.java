0ec3fa12d354bc1a83d426775860b526c38d110f
KurtisIvey <kurtiveycodes@gmail.com>
2023-09-26 20:34:37 -0500

    update

---
+++ src/September26/StringEndsWith.java
@@ -0,0 +1,18 @@
+package September26;
+
+public class StringEndsWith {
+    public static boolean solution(String str, String ending) {
+        // p full string, and ending of string that it should be a part of
+        // return boolean
+        /*
+            should be able to use a substring to calc where ending would be in a new string
+            then compare with ending to ensure it is .equals to
+         */
+        if (ending.length() > str.length()) {
+            return false;
+        }
+        // extracts the substring starting at the beginning of what the ending should be
+        String strEnding = str.substring(str.length() - ending.length());
+        return strEnding.equals(ending);
+    }
+}
